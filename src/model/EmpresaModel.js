1099bb68abba12958f0a850498adc5453838075a
JaislonDev <jaislon18@gmail.com>
2025-12-14 17:12:09 -0300

    Ultima atualização

--- src/model/EmpresaModel.js
+++ src/model/EmpresaModel.js
@@ -2,9 +2,9 @@ export class EmpresaModel
 {
     constructor(nome, cidade, estado)
     {
-        this.nome = nome;
-        this.cidade = cidade;
-        this.estado = estado;
+        this.setNome(nome);
+        this.setCidade(cidade);
+        this.setEstado(estado);
     }
 
     getNome()
@@ -14,6 +14,15 @@ export class EmpresaModel
 
     setNome(nome)
     {
+            if(!nome || nome.trim() ==="")
+        {
+            throw new Error("O nome da empresa é obrigatorio.");
+        } 
+
+            if (nome.length > 80)
+        {
+            throw new Error("O nome da empresa nao pode ter mais de 80 caracteres.");
+        }
         this.nome = nome;
     }
 
@@ -24,9 +33,20 @@ export class EmpresaModel
 
     setCidade(cidade)
     {
+            if(cidade.length > 45)
+        {
+            throw new Error("O nome da cidade não pode ter mais de 45 caracteres.");
+        } 
+
+            if(!cidade || cidade.trim() ==="")
+        {
+            throw new Error("O nome da cidade é obrigatorio.");
+        }  
+
         this.cidade = cidade;
     }
     
+
     getEstado()
     {
         return this.estado;
@@ -34,6 +54,16 @@ export class EmpresaModel
 
     setEstado(estado)
     {
+            if(estado.length > 45)
+        {
+            throw new Error("O nome do estado não pode ter mais de 45 caracteres");
+        }
+            if(!estado || estado.trim() ==="")
+
+        {
+            throw new Error("O nome do estado é obrigatorio.");
+        } 
+        
         this.estado = estado;
     }
 }
